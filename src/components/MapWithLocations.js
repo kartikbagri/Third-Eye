52c1c4be13df14267387ade2e8b9cc0fd1c415f5
Kartik Bagri <kartikbagri199@gmail.com>
2022-06-20 20:24:32 +0530

    map fix deployment

--- src/components/MapWithLocations.js
+++ src/components/MapWithLocations.js
@@ -1,8 +1,6 @@
 import Map, {FullscreenControl, GeolocateControl, NavigationControl, Marker} from 'react-map-gl';
-// import mapboxgl from "mapbox-gl"; // This is a dependency of react-map-gl even if you didn't explicitly install it
-
-// eslint-disable-next-line import/no-webpack-loader-syntax
-// mapboxgl.workerClass = require("worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker").default;
+import mapboxgl from 'mapbox-gl';
+mapboxgl.workerClass = require("worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker").default;
 
 const MapWithLocations = () => {
     return (
